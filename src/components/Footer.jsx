5248c309b2d20904dd76da50bd04c6c700d78d3a
nasquez23 <ivannaskov15@gmail.com>
2024-02-28 20:15:57 +0100

    finished markup on the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,45 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white p-4 mx-4">
-      <div className="w-3/4 m-auto flex flex-row justify-between text-center items-center p-4 pb-12 border-b-2">
-        <div className="">
-          <h3 className="text-xl">Premium Motors</h3>
-          <p className="text-sm">Location: 2220 Sveti Nikole, Macedonia</p>
-          <p className="text-sm">Phone: +389 00 000 000</p>
-          <p className="text-sm">Email: premiummotors@gmail.com</p>
+      <div className="w-3/4 m-auto flex flex-row justify-between text-center p-4 border-b-2 border-gray-600">
+        <div className="w-1/3">
+          <h3 className="text-2xl pb-4 text-bold text-left">Premium Motors</h3>
+          <p className="text-md py-2 text-left">
+            <FontAwesomeIcon icon={faMapMarkerAlt} className="pr-3" /> 2220 Sveti Nikole,
+            Macedonia
+          </p>
+          <p className="text-md py-2 text-left">
+            <FontAwesomeIcon icon={faPhone} className="pr-3" />+389 00 000 000
+          </p>
+          <p className="text-md py-2 text-left">
+            <FontAwesomeIcon icon={faEnvelope} className="pr-3" /> premiummotors@gmail.com
+          </p>
         </div>
-        <div>
-          <h3 className="text-xl">Our Product</h3>
-          <p className="text-sm">New Cars</p>
-          <p className="text-sm">Used Cars</p>
-          <p className="text-sm">Car Accessories</p>
+        <div className="w-1/4">
+          <h3 className="text-2xl pb-4 text-bold text-left">Our Product</h3>
+          <p className="text-md text-left py-1">New Cars</p>
+          <p className="text-md text-left py-1">Used Cars</p>
+          <p className="text-md text-left py-1">Car Accessories</p>
         </div>
-        <div>
-          <h3 className="text-xl">Follow Us</h3>
-          <p className="text-sm">Facebook</p>
-          <p className="text-sm">Instagram</p>
-          <p className="text-sm">Twitter</p>
+        <div className="w-1/4">
+          <h3 className="text-2xl pb-4 text-bold mb-2">Follow Us</h3>
+          <a href="http://www.facebook.com" target="_blank">
+            <FontAwesomeIcon icon={faFacebook} className="h-6" />
+          </a>
+          <a href="http://www.instagram.com" target="_blank" className="px-4">
+            <FontAwesomeIcon icon={faInstagram} className="h-6" />
+          </a>
+          <a href="http://www.twitter.com" target="_blank">
+            <FontAwesomeIcon icon={faTwitter} className="h-6" />
+          </a>
         </div>
       </div>
       <p className="w-3/4 m-auto text-sm p-4">
