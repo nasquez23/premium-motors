3cc1da5666df73786ee49438d06d66bb8765e3e4
nasquez23 <ivannaskov15@gmail.com>
2024-03-06 13:35:18 +0100

    small changes

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -9,13 +9,13 @@ export default function Cars() {
       <PageHeader title="Discover Your Dream Ride" />
       <div className="relative pt-6">
         <Title title="Cars For Sale" />
-        <div className="w-3/4 mx-auto grid grid-cols-3 gap-6 mt-20 mb-32">
+        <div className="w-3/4 mx-auto grid grid-cols-3 gap-8 mt-20 mb-40">
           {carsForSale.map((car, index) => (
             <CarCard key={index} car={car} />
           ))}
         </div>
         <Title title="Cars For Rent" />
-        <div className="w-3/4 mx-auto grid grid-cols-3 gap-10 mt-20 mb-32">
+        <div className="w-3/4 mx-auto grid grid-cols-3 gap-6 mt-20 mb-32">
           {carsForRent.map((car, index) => (
             <CarCard key={index} car={car} />
           ))}
