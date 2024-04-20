5b14325c87b290e92e09ffe9b09b3d6573fcde29
nasquez23 <ivannaskov15@gmail.com>
2024-04-20 16:43:40 +0200

    add password encryption

--- backend/src/controllers/users-controller.js
+++ backend/src/controllers/users-controller.js
@@ -1,5 +1,6 @@
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 const signup = async (req, res, next) => {
     const { name, email, password } = req.body;
@@ -15,10 +16,17 @@ const signup = async (req, res, next) => {
         return next(new HttpError("User exists already, please login instead.", 422));
     }
 
+    let hashedPassword;
+    try {
+        hashedPassword = await bcrypt.hash(password, 12);
+    } catch (error) {
+        return next(new HttpError("Could not create user, please try again.", 500));
+    }
+    
     const createdUser = new User({
         name,
         email,
-        password
+        password: hashedPassword
     });
 
     try {
@@ -40,7 +48,18 @@ const login = async (req, res, next) => {
         return next(new HttpError("Logging in failed, please try again later.", 500));
     }
 
-    if (!existingUser || existingUser.password !== password) {
+    if (!existingUser) {
+        return next(new HttpError("User doesn't exist.", 401));
+    }
+
+    let isValidPassword = false;
+    try {
+        isValidPassword = await bcrypt.compare(password, existingUser.password);
+    } catch (error) {
+        return next(new HttpError("Could not log you in, please check your credentials and try again.", 500));
+    }
+
+    if (!isValidPassword){
         return next(new HttpError("Invalid credentials, could not log you in.", 401));
     }
 
