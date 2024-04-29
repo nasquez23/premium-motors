dc47e89e627ef582a8bd5039dddc53958eee720a
nasquez23 <ivannaskov15@gmail.com>
2024-04-29 14:01:09 +0200

    Add not found page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Contact from "./pages/Contact";
 import Cars from "./pages/Cars";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/cars" element={<Cars />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </>
---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div className="text-gray-700 text-center mt-[10%] mb-[15%] max-lg:my-[32%]">
+            <h1 className="text-5xl font-bold mb-2">Page not found</h1>
+            <p className="text-2xl">The page you are looking for could not be found.</p>
+        </div>
+    );
+}
