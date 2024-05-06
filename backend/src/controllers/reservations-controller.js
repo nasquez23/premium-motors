2f8bc6543b5f32314bf54da4e1f92e2397f74b24
nasquez23 <ivannaskov15@gmail.com>
2024-05-06 20:20:45 +0200

    Add feature for sending emails for reservations

--- backend/src/controllers/reservations-controller.js
+++ backend/src/controllers/reservations-controller.js
@@ -1,5 +1,31 @@
 const HttpError = require("../models/http-error");
 const Reservation = require("../models/reservation");
+const nodemailer = require("nodemailer");
+
+const sendConfirmationEmail = async (reservationData) => {
+    const { name, email, date, car } = reservationData;
+
+    const transporter = nodemailer.createTransport({
+        service: "Gmail",
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    });
+
+
+    const mailOptions = {
+        from: process.env.EMAIL_USERNAME,
+        to: email,
+        subject: "Reservation Confirmation",
+        text: `Dear ${name},\n\nThank you for your reservation!\n\nDetails:\nCar: ${car}\nDate: ${date}\n\nBest regards,\nPremium Motors.`
+    };
+
+    await transporter.sendMail(mailOptions);
+};
 
 const addReservation = async (req, res, next) => {
     const { car, firstName, lastName, phone, age, email, address, city, zip, location, date } = req.body;
@@ -16,8 +42,10 @@ const addReservation = async (req, res, next) => {
         date
     });
 
+
     try {
         await newReservation.save();
+        await sendConfirmationEmail(newReservation);
         res.json({ message: "Reservation added" });
     } catch (error) {
         return next(new HttpError("Could not add reservation", 500));
